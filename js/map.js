07f4fc111bbc334c363ea402f593fe57dfbc3f50
JP <bonnerjp@gmail.com>
2013-06-03 11:39:46 +0800

    fixed map

--- js/map.js
+++ js/map.js
@@ -1,12 +1,16 @@
 function initialize() {
+  var point = new google.maps.LatLng(22.300, 114.168);
   var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8,
+    center: point,
+    zoom: 16,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map1 = new google.maps.Map(document.getElementById("map-canvas1"),
-      mapOptions);
-  var map2 = new google.maps.Map(document.getElementById("map-canvas2"),
-      mapOptions);
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: point,
+      map: map,
+      title: 'Point'
+  });
   }
 google.maps.event.addDomListener(window, 'load', initialize);
